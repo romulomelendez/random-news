7a06cdd1fa2581a359618f5c64ef010dbfe09f9e
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2024-01-09 18:18:56 -0300

    creating components some folders

---
+++ twitter-clone/src/app/components/new-post/new-post.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-post',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-post.component.html',
+  styleUrl: './new-post.component.sass'
+})
+export class NewPostComponent {
+
+}
