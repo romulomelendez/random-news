5f92abeaaf04e9c60aed9c5be43d5a45d47c2c61
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2024-01-09 19:39:21 -0300

    feat: creating home structure

--- twitter-clone/tailwind.config.js
+++ twitter-clone/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+		gridTemplateColumns: {
+			"main": "280px minmax(400px, 500px) 280px"
+		}
+	},
   },
   plugins: [],
 }
