87c4f1a607bb0a78bf77cd076ec5f038c0edfedb
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2024-01-10 07:05:35 -0300

    feat: sidebar menu buttons created

--- twitter-clone/src/app/components/left-sidebar/left-sidebar.component.ts
+++ twitter-clone/src/app/components/left-sidebar/left-sidebar.component.ts
@@ -1,11 +1,58 @@
 import { Component } from '@angular/core';
+import { MenuOptionComponent } from "../menu-option/menu-option.component";
+
+export type OptionsProps = {
+	path: string,
+	name: string,
+}
 
 @Component({
-  selector: 'app-left-sidebar',
-  standalone: true,
-  imports: [],
-  templateUrl: './left-sidebar.component.html'
+    selector: 'app-left-sidebar',
+    standalone: true,
+    templateUrl: './left-sidebar.component.html',
+    imports: [MenuOptionComponent]
 })
 export class LeftSidebarComponent {
-
+	icons: OptionsProps[] = [
+		{
+			path: "home",
+			name: "Home"
+		},
+		{
+			path: "search",
+			name: "Explore"
+		},
+		{
+			path: "notifications",
+			name: "Notifications"
+		},
+		{
+			path: "mail",
+			name: "Messages"
+		},
+		{
+			path: "featured_play_list",
+			name: "Lists"
+		},
+		{
+			path: "bookmark",
+			name: "Bookmarks"
+		},
+		{
+			path: "group",
+			name: "Communities"
+		},
+		{
+			path: "close",
+			name: "Premium"
+		},
+		{
+			path: "person",
+			name: "Profile"
+		},
+		{
+			path: "more_horiz",
+			name: "More"
+		},
+	];
 }
--- twitter-clone/tailwind.config.js
+++ twitter-clone/tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
     extend: {
 		gridTemplateColumns: {
 			"main": "280px minmax(400px, 500px) 280px"
+		},
+		textColor: {
+			"menu_light_gray": "#E7E9EA"
+		},
+		backgroundColor: {
+			"menu_button_hover": "#181818"
 		}
 	},
   },
