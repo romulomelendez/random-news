d89b5072897e14bb4a2ad383d2ee99670355eddf
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2024-01-23 16:31:14 -0300

    feat: Some header elements have been componentized

---
+++ twitter-clone/src/app/components/settings-button/settings-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+import { MatIconModule } from '@angular/material/icon'
+
+@Component({
+  selector: 'app-settings-button',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './settings-button.component.html'
+})
+export class SettingsButtonComponent {
+
+}
--- twitter-clone/src/app/components/top-menu/top-menu.component.ts
+++ twitter-clone/src/app/components/top-menu/top-menu.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core'
-import { MatIconModule } from '@angular/material/icon'
+import { SettingsButtonComponent } from '../settings-button/settings-button.component'
+import { TabMenuComponent } from '../tab-menu/tab-menu.component'
 import { XLogoComponent } from '../x-logo/x-logo.component'
 
 @Component({
 	selector: 'app-top-menu',
 	standalone: true,
 	templateUrl: './top-menu.component.html',
-	imports: [XLogoComponent, MatIconModule],
+	imports: [XLogoComponent, SettingsButtonComponent, TabMenuComponent],
 })
 export class TopMenuComponent {}
